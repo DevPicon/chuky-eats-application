3b67200dd8bba4ac94f5cd6425d9a38a63280bfe
Armando <picondev@gmail.com>
2020-10-31 20:24:04 -0300

    set title

--- app/src/main/java/pe/devpicon/android/compose/chukyeats/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/compose/chukyeats/MainActivity.kt
@@ -9,10 +9,16 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.ui.tooling.preview.Preview
 import pe.devpicon.android.compose.chukyeats.ui.MyChukyEatsApplicationTheme
 
@@ -43,19 +49,40 @@ fun MainApp() {
 
 @Composable
 fun UberTopBar() {
-    Box(modifier = Modifier
-        .background(Color.Red)
-        .height(60.dp)
-        .fillMaxWidth()
-    )
+    Row(
+        modifier = Modifier
+            .height(60.dp)
+            .fillMaxWidth(),
+        horizontalArrangement = Arrangement.Center,
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        Text(
+            text = stringResource(id = R.string.title_chuky),
+            style = TextStyle(
+                color = Color.Black,
+                fontSize = 28.sp
+            )
+        )
+        Spacer(modifier = Modifier.width(4.dp))
+        Text(
+            text = stringResource(id = R.string.title_eats),
+            style = TextStyle(
+                color = Color.Green,
+                fontSize = 28.sp,
+                fontWeight = FontWeight.Bold
+            )
+        )
+
+    }
 }
 
 @Composable
 fun NavigationBottomBar() {
-    Box(modifier = Modifier
-        .background(Color.Green)
-        .height(60.dp)
-        .fillMaxWidth()
+    Box(
+        modifier = Modifier
+            .background(Color.Green)
+            .height(60.dp)
+            .fillMaxWidth()
     )
 }
 
