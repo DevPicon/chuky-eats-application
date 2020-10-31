de00b057368d492c75753a2184bb9a2e3f18eab1
Bruno Aybar <miguel_bruno@hotmail.com>
2020-10-31 17:59:03 -0500

    Base app structure

--- app/src/main/java/pe/devpicon/android/compose/chukyeats/MainActivity.kt
+++ app/src/main/java/pe/devpicon/android/compose/chukyeats/MainActivity.kt
@@ -3,10 +3,16 @@ package pe.devpicon.android.compose.chukyeats
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Text
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.setContent
+import androidx.compose.ui.unit.dp
 import androidx.ui.tooling.preview.Preview
 import pe.devpicon.android.compose.chukyeats.ui.MyChukyEatsApplicationTheme
 
@@ -15,24 +21,48 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             MyChukyEatsApplicationTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                MainApp()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MainApp() {
+    Surface(
+        modifier = Modifier.fillMaxSize()
+    ) {
+        Scaffold(
+            topBar = { UberTopBar() },
+            bottomBar = { NavigationBottomBar() }
+        ) {
+
+        }
+    }
+}
+
+@Composable
+fun UberTopBar() {
+    Box(modifier = Modifier
+        .background(Color.Red)
+        .height(60.dp)
+        .fillMaxWidth()
+    )
+}
+
+@Composable
+fun NavigationBottomBar() {
+    Box(modifier = Modifier
+        .background(Color.Green)
+        .height(60.dp)
+        .fillMaxWidth()
+    )
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     MyChukyEatsApplicationTheme {
-        Greeting("Android")
+        MainApp()
     }
 }
