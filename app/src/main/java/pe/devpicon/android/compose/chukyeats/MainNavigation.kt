c44f2c543e4196fb0773915de6b2b7f84e938350
Armando <picondev@gmail.com>
2020-12-19 19:01:37 -0500

    use correct navigation bottom bar

--- app/src/main/java/pe/devpicon/android/compose/chukyeats/MainNavigation.kt
+++ app/src/main/java/pe/devpicon/android/compose/chukyeats/MainNavigation.kt
@@ -1,12 +1,13 @@
 package pe.devpicon.android.compose.chukyeats
 
 import androidx.compose.foundation.Text
-import androidx.compose.material.BottomNavigation
-import androidx.compose.material.BottomNavigationItem
-import androidx.compose.material.Scaffold
+import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Person
+import androidx.compose.material.icons.outlined.Person
+import androidx.compose.material.icons.outlined.Place
+import androidx.compose.material.icons.outlined.Search
+import androidx.compose.material.icons.outlined.ShoppingCart
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
@@ -14,11 +15,11 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigate
 import androidx.navigation.compose.rememberNavController
 import androidx.ui.tooling.preview.Preview
-import pe.devpicon.android.compose.chukyeats.feature.*
-import pe.devpicon.android.compose.chukyeats.feature.fooddetail.FoodDetailViewModel
+import pe.devpicon.android.compose.chukyeats.feature.HomeScreen
 import pe.devpicon.android.compose.chukyeats.feature.fooddetail.FoodScreenNavigatable
 import pe.devpicon.android.compose.chukyeats.feature.profile.ProfileScreen
 import pe.devpicon.android.compose.chukyeats.ui.MyChukyEatsApplicationTheme
+import pe.devpicon.android.compose.chukyeats.ui.greenChuky
 
 @Composable
 fun MainNavigation() {
@@ -42,28 +43,61 @@ fun MainNavigation() {
 
 @Composable
 fun NavigationBottomBar(navController: NavController) {
-    BottomNavigation {
+    BottomNavigation(
+        backgroundColor = MaterialTheme.colors.surface
+    ) {
         BottomNavigationItem(
             label = { Text("Home") },
-            icon = { Icons.Default.Home },
-            selected = false,
+            icon = {
+                Icon(
+                    asset = Icons.Filled.Home,
+                    tint = greenChuky
+                )
+            },
+            selected = true,
             onClick = { navController.navigate("home") }
         )
         BottomNavigationItem(
-            label = { Text("Profile") },
-            icon = { Icons.Default.Person },
+            label = { Text("Place") },
+            icon = {
+                Icon(
+                    asset = Icons.Outlined.Place,
+                    tint = MaterialTheme.colors.onSurface
+                )
+            },
+            selected = false,
+            onClick = { navController.navigate("profile") }
+        )
+        BottomNavigationItem(
+            label = { Text("Search") },
+            icon = {
+                Icon(
+                    asset = Icons.Outlined.Search,
+                    tint = MaterialTheme.colors.onSurface
+                )
+            },
             selected = false,
             onClick = { navController.navigate("profile") }
         )
         BottomNavigationItem(
             label = { Text("Detail") },
-            icon = { Icons.Default.Person },
+            icon = {
+                Icon(
+                    asset = Icons.Outlined.ShoppingCart,
+                    tint = MaterialTheme.colors.onSurface
+                )
+            },
             selected = false,
             onClick = { navController.navigate("food/1") }
         )
         BottomNavigationItem(
             label = { Text("Detail 2") },
-            icon = { Icons.Default.Person },
+            icon = {
+                Icon(
+                    asset = Icons.Outlined.Person,
+                    tint = MaterialTheme.colors.onSurface
+                )
+            },
             selected = false,
             onClick = { navController.navigate("food/2") }
         )
